9c42929382955e7176ee1c56638f0276524f51d4
uuuut0 <u42t00@gmail.com>
2024-05-31 18:11:29 +0800

    feat:vue 共用元件建立HComp_SectionTitle,HComp_LearnMoreBtn,首頁區塊階段完成(SectionNews.vue)

--- src/components/home/SectionNews.vue
+++ src/components/home/SectionNews.vue
@@ -1,50 +1,71 @@
+<script setup>
+import HComp_SectionTitle from './HComp_SectionTitle.vue';
+import HComp_LearnMoreBtn from './HComp_LearnMoreBtn.vue';
+</script>
 <template>
     <section class="home-section-news page-section">
         <div class="container">
+            <HComp_SectionTitle title="最新消息"/>
             <div class="hs-news-items-wrap">
-                <div class="hs-news-items">
+                <div class="hs-news-items" v-for="n in 4" :key="n">
                     <div class="hs-news-item">
-                        <span class="hs-news-published-date">
+                        <span class="hs-news-published-date col-2">
                             2022/05/18
                         </span>
-                        <p class="hs-news-item-title">
-                            <span class="hs-news-item-title-category">限時優惠</span>
+                        <p class="hs-news-item-title col-10">
+                            <span class="hs-news-item-title-category col-1">限時優惠</span>
                             樂園門票、國外網路卡限時特價，立即搶購，享受更多折扣
                         </p>
                     </div>
                 </div>
             </div>
+            <HComp_LearnMoreBtn/>
         </div>
     </section>
+    <HComp_SectionTitle title="票券"/>
+    <HComp_SectionTitle title="標題文字測試"/>
 </template>
 
-<script></script>
+<script>
+
+</script>
 
 <style lang="scss" scoped>
 @import '../../assets/styles/base/color';
 @import '../../assets/styles/base/font';
 
-
-h3{
-    color: $black;
+// "col-12 col-md-6 col-lg-4 col-xl-3"
+section{
     background-color: $secondColor-1;
+    // min-height: 300px;
+    font-size: $base-fontSize;
+    color: $black;
+    padding: 50px 0
 }
+
 .hs-news-item{
     display: flex;
     position: relative;
-    padding: 20px 0px;
+    padding: $base-fontSize 0px;
+    width: 100%;
+    border-radius: 10px;
+    background-color: $primaryColor;
+    margin-bottom: $base-fontSize;
 }
+
 .hs-news-item-title-category{
     display: inline-block;
     line-height: 120%;
-    // margin-right: 100px;
     border-right: 1px solid $black;
-    background-color: #776e6e;
+    text-align: center;
+
 }
 .hs-news-published-date{
     display: inline-block;
-    padding: 0 12px;
+    // padding: 0 12px;
     // background-color: #b4df73;
     line-height: 120%;
+    text-align: center;
+
 }
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,9 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
 import SectionNews from '../components/home/SectionNews.vue'
 </script>
 
 <template>
   <main>
-    <!-- <TheWelcome /> 用不到我先註解-->
     <SectionNews />
   </main>
 </template>
