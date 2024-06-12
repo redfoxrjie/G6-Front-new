deeb7a50edf539edadf90a3ac70dcdaac1a3f454
NaBi0215 <cocoshzmiki@gmail.com>
2024-06-12 16:36:00 +0800

    feat:票券一覽+聯絡我們(RWD)

--- src/views/ContactView.vue
+++ src/views/ContactView.vue
@@ -1,6 +1,4 @@
 <template>
-    <!-- <main>目前位在「票券訂購」</main> -->
-    <!-- 上方導覽列用共用元件? -->
     <h3>聯絡我們</h3>
     <section>
         <div class="contactus_pic">
@@ -8,42 +6,43 @@
             <img class="leftHand" src="../assets/images/contactusDog_03.png" alt="">
             <img class="rightHand" src="../assets/images/contactusDog_02.png" alt="">
         </div>
-        <div class="container">
-            <form id="contactus">
-            <div class="form_group form_name">  
-                <label for="name">姓名</label>
-                <input type="text" id="name">
-            </div>
-            <div class="form_group">
-                <label for="tel">聯絡電話</label>
-                <input type="tel" id="tel">
-            </div>
-            <div class="form_group">
-                <label for="email">e-mail</label>
-                <input for="email" id="email">
-            </div>
-            <div class="form_group form_choice">
-                <p>想詢問項目</p>
-                {{ checkbox }}<br />
-                <label >
-                <input type="checkbox" value="0" v-model="checkbox" />介面操作
-                <!-- <span class="coverStyle"></span> -->
-                </label>
-                <label >
-                <input type="checkbox" value="1" v-model="checkbox" />已購入票券
-                </label>
-                <label >
-                <input type="checkbox" value="2" v-model="checkbox" />其他
-                </label>
-            </div>
-            <div class="form_group">
-                <label for="message"></label>
-                <textarea id="message" placeholder="想詢問什麼呢"></textarea>
-            </div>
-            </form>
-        </div>
+
+        <table class="container">
+            <tbody id="contactus">
+                <tr class="form_group form_name">  
+                    <td>姓名</td>
+                    <td><input type="text" id="name"></td>
+                </tr>
+                <tr class="form_group">
+                    <td>聯絡電話</td>
+                    <td><input type="tel" id="tel"></td>
+                </tr>
+                <tr class="form_group">
+                    <td>e-mail</td>
+                    <td><input for="email" id="email"></td>
+                </tr>
+                <div class="form_group form_choice">
+                    <p>想詢問項目</p>
+                    {{ checkbox }}<br />
+                    <label >
+                    <input type="checkbox" value="0" v-model="checkbox" />介面操作
+                    <!-- <span class="coverStyle"></span> -->
+                    </label>
+                    <label >
+                    <input type="checkbox" value="1" v-model="checkbox" />已購入票券
+                    </label>
+                    <label >
+                    <input type="checkbox" value="2" v-model="checkbox" />其他
+                    </label>
+                </div>
+                <div class="form_group">
+                    <label for="message"></label>
+                    <textarea id="message" placeholder="想詢問什麼呢"></textarea>
+                </div>
+            </tbody>
+        </table>
         <div class="btn">
-            <button>確認送出</button>
+            <button class="btn-1">確認送出</button>
         </div>
     </section>
   </template>
@@ -70,101 +69,99 @@
     }
     .contactus_pic{
         position: relative;
+        .head{
+            width: 20%;
+            position: absolute;
+            left: 40%;
+            bottom: 0%;
+        }
+        .leftHand{
+            width: 8%;
+            position: absolute;
+            left: 40%;
+            bottom: -40px;
+            z-index: 5;
+        }
+        .rightHand{
+            width: 8%;
+            position: absolute;
+            left: 52%;
+            bottom: -40px;
+            z-index: 5;
+        }
+        @media (max-width: 768px) {
+            .head{
+                width: 50%;
+                left: 25%;
+            }
+            .leftHand{
+                width: 20%;
+                position: absolute;
+                left: 25%;
+                z-index: 5;
+            }
+            .rightHand{
+                width: 20%;
+                position: absolute;
+                left: 55%;
+                bottom: -40px;
+                z-index: 5;
+            }
+        }
     }
-    .head{
-        width: 20%;
-        position: absolute;
-        left: 40%;
-        bottom: 0%;
-    }
-    .leftHand{
-        width: 8%;
-        position: absolute;
-        left: 40%;
-        bottom: -40px;
-        z-index: 5;
-    }
-    .rightHand{
-        width: 8%;
-        position: absolute;
-        left: 52%;
-        bottom: -40px;
-        z-index: 5;
-    }
+
     .container{
         // outline: 1px solid red;
         display: flex;
         justify-content: center;
-    }
-    #contactus{
-        display: flex;
-        justify-content: center;
-        flex-direction: column;
-        height: auto;
-        width: 450px;
-        // outline: 1px solid red;
-        border-radius: 30px;
-        background-color: #E2F1FF;
-        position: relative;
-    }
-  // input[type="checkbox"]{
-  //   display: none;
-  // }
-  // .coverStyle{
-  //   display: inline-block;
-  //   width: 15px;
-  //   height: 15px;
-  //   border: 1px solid;
-  //   background-color: white;
-  //   border-radius: 200px;
-  //   margin-right: 10px;
-  //   vertical-align: middle;
-  // }
-  // input[type="checkbox"]:checked + .coverStyle::after {
-  //   content: '\2714';
-  //   font-size: 12px; /* 调整字体大小以适应容器 */
-  //   color: #727272;
-  //   text-align: center;
-  //   line-height: 15px; /* 匹配容器高度 */
-  //   position: absolute;
-  //   top: 50%;
-  //   left: 50%;
-  //   transform: translate(-50%, -50%);
-  // }
-    .form_group{
-        padding: 0px 80px;
-        padding-top: 50px;
-    }
-    .form_name{
-        padding-top: 100px;
-    }
-    .form_choice p{
-        color:$secondColor-2;
-        padding-bottom: 10px;
-    }
-    .form_group label{
-        color: $secondColor-2;
-        padding-right: 5px;
-    }
-    .form_group input{
-        border: 1px solid $secondColor-1;
-        width: auto;
-        height: 20px;
-        border-radius: 5px;
-    }
-    .form_group textarea{
-        padding: 10px;
-        margin-bottom: 50px;
-        height: 200px;
-        width: 100%;
-        // box-sizing: border-box;
-        border: 1px solid $secondColor-1;
-        border-radius: 5px;
-        resize: none;
+        #contactus{
+            display: flex;
+            justify-content: center;
+            flex-direction: column;
+            height: auto;
+            width: 450px;
+            // outline: 1px solid red;
+            border-radius: 30px;
+            background-color: #E2F1FF;
+            position: relative;
+            .form_group{
+                padding: 0px 80px;
+                padding-top: 50px;
+                color:$secondColor-2;
+            }
+            .form_name{
+                padding-top: 100px;
+            }
+            .form_choice p{
+                color:$secondColor-2;
+                padding-bottom: 10px;
+            }
+            .form_group label{
+                color: $secondColor-2;
+                padding-right: 5px;
+            }
+            .form_group input{
+                border: 1px solid $secondColor-1;
+                width: auto;
+                height: 20px;
+                border-radius: 5px;
+                // padding: 0px 20px;
+            }
+            .form_group textarea{
+                padding: 10px;
+                margin-bottom: 50px;
+                height: 200px;
+                width: 100%;
+                // box-sizing: border-box;
+                border: 1px solid $secondColor-1;
+                border-radius: 5px;
+                resize: none;
+            }
+        }
     }
     .btn{
         text-align: center;
         margin: 20px 0px;
     }
-  </style>
+</style>
     
--- src/views/TicketsView.vue
+++ src/views/TicketsView.vue
@@ -1,87 +1,84 @@
 <template>
-  <div class="tickets-container">
-    <div v-for="(ticket, index) in tickets" :key="index" class="ticket">
-      <img :src="ticket.image" :alt="ticket.title" class="ticket-image" />
-      <h3 class="ticket-title">{{ ticket.title }}</h3>
-      <p class="ticket-description">{{ ticket.description }}</p>
+  <div class="Ticket_Section">
+    <h2>票券一覽</h2>
+    <h3 class="Area_TicketTitle">日本-沖繩</h3>
+    <div class="TicketCard_Wrapper row row-cols-1 row-cols-md-2 row-cols-lg-3">
+      <div class="Card_Container col" v-for="ticket in okinawaTickets" :key="ticket.id">
+        <div class="ticket_Card">
+          <div class="ticket-img">
+            <img :src="ticket.image" :alt="ticket.name" />
+          </div>
+          <div class="Ticket_Txt">
+            <h4>{{ ticket.name }}</h4>
+            <p>{{ ticket.content }}</p>
+            <p class="TicketPrice">{{ ticket.price }}</p>
+            <p>{{ ticket.viewers }}</p>
+          </div>
+        </div>
+      </div>
     </div>
-  </div>
-</template>
-<script>
-import ticket1 from '@/assets/images/ticket1.jpg';
-import ticket2 from '@/assets/images/ticket_02.jpg';
-import ticket3 from '@/assets/images/ticket_03.jpg';
-import ticket4 from '@/assets/images/ticket_04.jpg';
-import ticket5 from '@/assets/images/ticket_05.jpg';
-import ticket6 from '@/assets/images/ticket_06.jpg';
-import ticket7 from '@/assets/images/ticket_07.jpg';
-import ticket8 from '@/assets/images/ticket_08.jpg';
-import ticket9 from '@/assets/images/ticket_09.jpg';
-import ticket10 from '@/assets/images/ticket_10.jpg';
-import ticket11 from '@/assets/images/ticket_11.jpg';
-import ticket12 from '@/assets/images/ticket_12.jpg';
-import ticket13 from '@/assets/images/ticket_13.jpg';
-import ticket14 from '@/assets/images/ticket_14.jpg';
-import ticket15 from '@/assets/images/ticket15.jpg';
-
-export default {
-  data() {
-    return {
-      tickets: [
-        { image: ticket1, title: 'Title 1', description: 'Description 1' },
-        { image: ticket2, title: 'Title 2', description: 'Description 2' },
-        { image: ticket3, title: 'Title 3', description: 'Description 3' },
-        { image: ticket4, title: 'Title 4', description: 'Description 4' },
-        { image: ticket5, title: 'Title 5', description: 'Description 5' },
-        { image: ticket6, title: 'Title 6', description: 'Description 6' },
-        { image: ticket7, title: 'Title 7', description: 'Description 7' },
-        { image: ticket8, title: 'Title 8', description: 'Description 8' },
-        { image: ticket9, title: 'Title 9', description: 'Description 9' },
-        { image: ticket10, title: 'Title 10', description: 'Description 10' },
-        { image: ticket11, title: 'Title 11', description: 'Description 11' },
-        { image: ticket12, title: 'Title 12', description: 'Description 12' },
-        { image: ticket13, title: 'Title 13', description: 'Description 13' },
-        { image: ticket14, title: 'Title 14', description: 'Description 14' },
-        { image: ticket15, title: 'Title 15', description: 'Description 15' }
-      ]
-    }
-  }
-}
-</script>
-<!-- <script>
-export default {
-  data() {
-    return {
-      tickets: [
-        { image: require('@/assets/images/ticket1.jpg'), title: 'Title 1', description: 'Description 1' },
-        { image: require('@/assets/images/ticket_02.jpg'), title: 'Title 2', description: 'Description 2' },
-        { image: require('@/assets/images/ticket_03.jpg'), title: 'Title 3', description: 'Description 3' },
-        { image: require('@/assets/images/ticket_04.jpg'), title: 'Title 4', description: 'Description 4' },
-        { image: require('@/assets/images/ticket_05.jpg'), title: 'Title 5', description: 'Description 5' },
-        { image: require('@/assets/images/ticket_06.jpg'), title: 'Title 6', description: 'Description 6' },
-        { image: require('@/assets/images/ticket_07.jpg'), title: 'Title 7', description: 'Description 7' },
-        { image: require('@/assets/images/ticket_08.jpg'), title: 'Title 8', description: 'Description 8' },
-        { image: require('@/assets/images/ticket_09.jpg'), title: 'Title 9', description: 'Description 9' },
-        { image: require('@/assets/images/ticket_10.jpg'), title: 'Title 10', description: 'Description 10' },
-        { image: require('@/assets/images/ticket_11.jpg'), title: 'Title 11', description: 'Description 11' },
-        { image: require('@/assets/images/ticket_12.jpg'), title: 'Title 12', description: 'Description 12' },
-        { image: require('@/assets/images/ticket_13.jpg'), title: 'Title 13', description: 'Description 13' },
-        { image: require('@/assets/images/ticket_14.jpg'), title: 'Title 14', description: 'Description 14' },
-        { image: require('@/assets/images/ticket15.jpg'), title: 'Title 15', description: 'Description 15' }
-      ]
-    }
-  }
-}
-</script> -->
-
-
-<!-- <template>
-  <div class="carousel-container">
-    <div class="owl-carousel owl-theme">
-      <div class="item" v-for="(item, index) in items" :key="index">
-        <img :src="item.image" :alt="item.title">
-        <h4>{{ item.title }}</h4>
-        <p>{{ item.description }}</p>
+    <h3 class="Area_TicketTitle">日本-大阪</h3>
+    <div class="TicketCard_Wrapper row row-cols-1 row-cols-md-2 row-cols-lg-3">
+      <div class="Card_Container col" v-for="ticket in osakaTickets" :key="ticket.id">
+        <div class="ticket_Card">
+          <div class="ticket-img">
+            <img :src="ticket.image" :alt="ticket.name" />
+          </div>
+          <div class="Ticket_Txt">
+            <h4>{{ ticket.name }}</h4>
+            <p>{{ ticket.content }}</p>
+            <p class="TicketPrice">{{ ticket.price }}</p>
+            <p>{{ ticket.viewers }}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+    <h3 class="Area_TicketTitle">泰國-曼谷</h3>
+    <div class="TicketCard_Wrapper row row-cols-1 row-cols-md-2 row-cols-lg-3">
+      <div class="Card_Container col" v-for="ticket in thailandTickets" :key="ticket.id">
+        <div class="ticket_Card">
+          <div class="ticket-img">
+            <img :src="ticket.image" :alt="ticket.name" />
+          </div>
+          <div class="Ticket_Txt">
+            <h4>{{ ticket.name }}</h4>
+            <p>{{ ticket.content }}</p>
+            <p class="TicketPrice">{{ ticket.price }}</p>
+            <p>{{ ticket.viewers }}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+    <h3 class="Area_TicketTitle">韓國-首爾</h3>
+    <div class="TicketCard_Wrapper row row-cols-1 row-cols-md-2 row-cols-lg-3">
+      <div class="Card_Container col" v-for="ticket in koreaTickets" :key="ticket.id">
+        <div class="ticket_Card">
+          <div class="ticket-img">
+            <img :src="ticket.image" :alt="ticket.name" />
+          </div>
+          <div class="Ticket_Txt">
+            <h4>{{ ticket.name }}</h4>
+            <p>{{ ticket.content }}</p>
+            <p class="TicketPrice">{{ ticket.price }}</p>
+            <p>{{ ticket.viewers }}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+    <h3 class="Area_TicketTitle">越南-富國島</h3>
+    <div class="TicketCard_Wrapper row row-cols-1 row-cols-md-2 row-cols-lg-3">
+      <div class="Card_Container col" v-for="ticket in vietnamTickets" :key="ticket.id">
+        <div class="ticket_Card">
+          <div class="ticket-img">
+            <img :src="ticket.image" :alt="ticket.name" />
+          </div>
+          <div class="Ticket_Txt">
+            <h4>{{ ticket.name }}</h4>
+            <p>{{ ticket.content }}</p>
+            <p class="TicketPrice">{{ ticket.price }}</p>
+            <p>{{ ticket.viewers }}</p>
+          </div>
+        </div>
       </div>
     </div>
   </div>
@@ -89,34 +86,94 @@ export default {
 
 <script>
 export default {
-  name: 'CarouselComponent',
   data() {
     return {
-      items: [
-        { image: '../assets/images/ticket_01.jpg', title: 'Image 1', description: 'Description 1' },
-        { image: '../assets/images/ticket_01.jpg', title: 'Image 2', description: 'Description 2' },
-        { image: '../assets/images/ticket_01.jpg', title: 'Image 3', description: 'Description 3' }
-      ]
+      tickets: []
+    };
+  },
+  computed: {
+    okinawaTickets() {
+      return this.tickets.filter(ticket => [1, 2, 3].includes(ticket.id));
+    },
+    osakaTickets() {
+      return this.tickets.filter(ticket => [4, 5, 6].includes(ticket.id));
+    },
+    koreaTickets() {
+      return this.tickets.filter(ticket => [7, 8, 9].includes(ticket.id));
+    },
+    thailandTickets() {
+      return this.tickets.filter(ticket => [10, 11, 12].includes(ticket.id));
+    },
+    vietnamTickets() {
+      return this.tickets.filter(ticket => [13, 14, 15].includes(ticket.id));
     }
   },
   mounted() {
-    this.initOwlCarousel();
+    this.loadJsonData();
   },
   methods: {
-    initOwlCarousel() {
-      // 等待 DOM 完全渲染後初始化 OwlCarousel
-      this.$nextTick(() => {
-        $('.owl-carousel').owlCarousel({
-          items: 1,
-          loop: true,
-          nav: true,
-          dots: true,
-          autoplay: true,
-          autoplayTimeout: 3000,
-          autoplayHoverPause: true
+    loadJsonData() {
+      fetch('/json/tickets.json')
+        .then(response => response.json())
+        .then(data => {
+          this.tickets = data.tickets;
+        })
+        .catch(error => {
+          console.error('Error loading JSON data:', error);
         });
-      });
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+@import '@/assets/styles/base/color';
+@import '@/assets/styles/base/font';
+
+.Ticket_Section {
+  padding: 100px;
+  h2{
+    margin-top: 100px;
+    margin-bottom: 80px;
+    text-align: center;
+    color: $secondColor-2;
+  }
+  .Area_TicketTitle{
+    padding: 30px 0px;
+    color: $secondColor-2;
+  }
+}
+
+.TicketCard_Wrapper {
+  display: flex;
+  flex-wrap: wrap;
+  .ticket_Card {
+    padding: 20px;
+    .ticket-img img { 
+    width: 90%;
+    border-radius: 80px;
+    box-shadow: 0 4px 12px rgba(0,0,0,.5);
+    cursor: pointer;
+    transform: translate(0, -10%);
+    transition: scale .5s ease-in-out;
+    &:hover{
+        scale: 1.05;
+        transition: scale .5s ease-in-out;
+    }
+  }
+    .Ticket_Txt {
+      padding: 10px 20px;
+      color: $gray;
+      cursor: pointer;
+      .TicketPrice{
+        color: $secondColor-2;
+      }
+      &:hover {
+        color: $secondColor-2;
+      }
     }
   }
 }
-</script> -->
+
+
+</style>
